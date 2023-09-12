965dc5efb1e074efded377f22b47b7d28671a8d5
octocatblain <blainmasaku2@gmail.com>
2023-09-12 14:18:37 +0300

    add fonts

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,4 +1,4 @@
-<div class="container">
+<div class="footer">
     <div class="row justify-content-around">
         <div class="col-6 text-center">
             <p>Project period: 1st - 11th September 2023</p>
@@ -8,3 +8,10 @@
         </div>
     </div>
 </div>
+<footer data-scroll-section="true" class="footer" data-scroll-section-id="section2"
+    style="opacity: 1; pointer-events: all; transform: matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, -2281, 0, 1);"
+    data-scroll-section-inview="">
+    <p class="footer__p">Alexxandria Forsque - is an observer. Her sensitivity and natural intuition make her the perfect
+        woman to direct and capture life’s real characters.</p><img class="footer__copyright"
+        src="/images/adeola-credit.svg" alt="designed and developed by adeola">
+</footer>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,16 +8,19 @@
     <title>@yield('title') | Support</title>
 
     <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ruwudu:wght@400;500;600;700&display=swap" rel="stylesheet">
     <!-- Styles -->
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-    @livewireStyles()
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Ruwudu:wght@400;500;600;700&display=swap');
+    </style>
+    {{-- @livewireStyles() --}}
 
     @livewireScripts()
 </head>
