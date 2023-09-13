56e4685a74aedb6136963f057e02df8786ed67ef
octocatblain <blainmasaku2@gmail.com>
2023-09-13 15:20:08 +0300

    Feature: add footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,17 +1,23 @@
-<div class="footer">
-    <div class="row justify-content-around">
-        <div class="col-6 text-center">
-            <p>Project period: 1st - 11th September 2023</p>
-        </div>
-        <div class="col-6 text-center">
-            <p>Made by Blain Muema</p>
-        </div>
+<hr style="color: #000; height: 1px;">
+
+<div class="row " style="font-size: smaller;">
+    <div class="col-lg-2" style="display: flex; justify-content: space-between;">
+        <p class="mb-0"><a wire:navigate href="booking.php">Terms</a></p>
+        <p class="mb-0"><a wire:navigate href="#">Privacy</a></p>
+        <p class="mb-0"><a wire:navigate href="#">Security</a></p>
     </div>
+
+    <div class="col-10 " style="display: flex; justify-content: flex-end;">
+        <p class="mb-0"><a wire:navigate href="/">Design &amp; Development</a> &copy; 2023 -
+            <script>
+                document.write(new Date().getFullYear());
+            </script> All Rights Reserved &mdash; <a wire:navigate
+                href="https://octocatblain.github.io">Blain Muema</a>
+        </p>
+    </div>
+</div> <!-- /.container -->
+
+{{-- back to top button --}}
+<div>
+    <a wire:navigate href="#" id="back-to-top" title="Back to top">&uarr;</a>
 </div>
-<footer data-scroll-section="true" class="footer" data-scroll-section-id="section2"
-    style="opacity: 1; pointer-events: all; transform: matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, -2281, 0, 1);"
-    data-scroll-section-inview="">
-    <p class="footer__p">Alexxandria Forsque - is an observer. Her sensitivity and natural intuition make her the perfect
-        woman to direct and capture life’s real characters.</p><img class="footer__copyright"
-        src="/images/adeola-credit.svg" alt="designed and developed by adeola">
-</footer>
