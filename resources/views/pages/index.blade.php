cd0d0edd1659e2dbdae0be7cc7d806cc257cc74a
Blain Muema <blainmuema@gmail.com>
2023-10-02 10:45:06 +0300

    update composer

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
 
 @section('title')
-    {{$title}}
+    {{ $title }}
 @endsection
 
 @section('content')
-    <div class="container">
+    <div class="container ">
         <div class="row justify-content-center">
             <div class="col-6 p-2">
                 @livewire('tickets') </div>
