c89c805e857da54193653c9b402f56b089092e31
Blain Muema <blainmuema@gmail.com>
2023-10-02 10:44:41 +0300

    update livewire component name

--- resources/views/livewire/logout.blade.php
+++ resources/views/livewire/logout.blade.php
@@ -1,3 +1,3 @@
 <li class="nav-item ">
-    <a class="nav-link text-white" wire:navigate wire:click="Logout" href="/">Logout</a>
+    <a class="nav-link text-white" wire:navigate wire:click="logout" href=" ">Logout</a>
 </li>
