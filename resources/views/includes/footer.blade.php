e44f35fdf6ab7aa7316e3507a7f01a69134a8c3c
Blain Muema <blainmuema@gmail.com>
2023-10-02 10:43:36 +0300

    feat: update footer content and alignment

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,23 +1,23 @@
-<hr style="color: #000; height: 1px;">
+<div class="section">
+    <hr class="mx-5" style="color: #000; height: 1px;">
 
-<div class="row " style="font-size: smaller;">
-    <div class="col-lg-2" style="display: flex; justify-content: space-between;">
-        <p class="mb-0"><a wire:navigate href="booking.php">Terms</a></p>
-        <p class="mb-0"><a wire:navigate href="#">Privacy</a></p>
-        <p class="mb-0"><a wire:navigate href="#">Security</a></p>
+
+
+    <div class="row font-sm px-5 d-flex align-item-center justify-content-center">
+
+        <div class="col-6 d-flex justify-content-between">
+            <p class="mb-0"><a wire:navigate href="booking.php">Terms</a></p>
+            <p class="mb-0"><a wire:navigate href="#">Privacy</a></p>
+            <p class="mb-0"><a wire:navigate href="#">Security</a></p>
+            <p> | </p>
+            <p class="mb-0">&copy; Aug. 2023</p>
+            <p><a wire:navigate href="https://octocatblain.github.io">Blain Muema</a></p>
+        </div>
+    </div> <!-- /.container -->
+
+    {{-- back to top button --}}
+    <div>
+        <a wire:navigate href="#" id="back-to-top" title="Back to top">&uarr;</a>
     </div>
 
-    <div class="col-10 " style="display: flex; justify-content: flex-end;">
-        <p class="mb-0"><a wire:navigate href="/">Design &amp; Development</a> &copy; 2023 -
-            <script>
-                document.write(new Date().getFullYear());
-            </script> All Rights Reserved &mdash; <a wire:navigate
-                href="https://octocatblain.github.io">Blain Muema</a>
-        </p>
-    </div>
-</div> <!-- /.container -->
-
-{{-- back to top button --}}
-<div>
-    <a wire:navigate href="#" id="back-to-top" title="Back to top">&uarr;</a>
 </div>
